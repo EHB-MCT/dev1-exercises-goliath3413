48618357ad52ceb214c564eb763e49afcd738c06
test naam <fake-email@test.be>
2022-10-05 16:43:52 +0200

    ,

--- exercises/1-functions/1-1-name.js
+++ exercises/1-functions/1-1-name.js
@@ -8,6 +8,7 @@ let context = canvas.getContext('2d');
 //idk what im doing//
 drawNameG();
 function drawNameG(){
+    //G
     context.lineWidth = 5;
     context.strokeStyle = 'red';
     context.beginPath();
@@ -19,7 +20,7 @@ context.lineTo(150, 200);
 context.lineTo(150, 155);
 context.lineTo(125, 155);
 context.stroke();
-
+//E
 context.strokeStyle = 'blue';
 context.beginPath();
 context.moveTo(200, 100);
@@ -29,9 +30,18 @@ context.lineTo(200, 200);
 context.moveTo(250, 150);
 context.lineTo(200, 150);
 context.stroke();
-
+//O
 context.strokeStyle = 'yellow';
 context.beginPath();
-context.moveTo(350, 100);
-context.lineTo()
+context.arc(350, 150, 50, 0, 2 * Math.PI);
+context.stroke()
+
+//R
+context.strokeStyle = "brown";
+context.beginPath();
+context.moveTo(450, 100);
+context.lineTo(450, 200);
+context.moveTo(450, 100);
+context.arc(450, 100, 0, 450, 150,);
+context.stroke();
 }
